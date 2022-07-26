51abb6b7b3d69d5ed2f6d74257c700a69fd60304
chudroy <nijotigajo@gmail.com>
2022-07-26 16:02:19 +0200

    neighbourhood filter navbar pain

---
+++ public/javascripts/neighbourhoods.js
@@ -0,0 +1,69 @@
+module.exports.neighbourhoods = [
+  {
+    district: "Deusto",
+    neighbourhoods: [
+      "Arangoiti",
+      "Ibarrekolanda",
+      "San Inazio-Elorrieta",
+      "Deusto",
+    ],
+  },
+  {
+    district: "Uríbarri",
+    neighbourhoods: [
+      "Castaños",
+      "Matico-Ciudad Jardín",
+      "Uribarri",
+      "Zurbaran-Arabella",
+    ],
+  },
+  {
+    district: "Otxarkoaga-Txurdinaga",
+    neighbourhoods: ["Otxarkoaga", "Txurdinaga"],
+  },
+  {
+    district: "Begoña",
+    neighbourhoods: ["Begoña", "Bolueta", "Santutxu"],
+  },
+  {
+    district: "Ibaiondo",
+    neighbourhoods: [
+      "Atxuri",
+      "Bilbao La Vieja",
+      "Casco Viejo",
+      "Iturralde",
+      "La Peña",
+      "Miribilla",
+      "San Adrian",
+      "San Francisco",
+      "Buia",
+      "Solokoetxe",
+      "Zabala",
+    ],
+  },
+  {
+    district: "Abando",
+    neighbourhoods: ["Abando", "Indautxu", "Abandoibarra"],
+  },
+  {
+    district: "Recalde",
+    neighbourhoods: [
+      "Ametzola",
+      "Iralabarri",
+      "Iturrigorri-Peñascal",
+      "Rekaldeberri-Larrasquitu",
+      "Uretamendi",
+      "Betolaza",
+    ],
+  },
+  {
+    district: "Basurto-Zorroza",
+    neighbourhoods: [
+      "Altamira",
+      "Basurto",
+      "Olabeaga",
+      "Masustegui",
+      "Zorroza",
+    ],
+  },
+];
